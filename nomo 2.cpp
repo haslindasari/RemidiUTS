3dff35b198263800565cb2c93827fa5b54390ead
haslindasari <49137250+haslindasari@users.noreply.github.com>
2019-04-01 16:54:47 +0700

    Create nomo 2.cpp

---
+++ nomo 2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout<<"Nama             : Haslinda Sari"<<endl;
+  cout<<"NIM              : 1829140005"<<endl;
+  cout<<"Kelas            : Tekom B"<<endl;
+  cout<<"Tahun Angkatan   : 2018"<<endl;
+  
+  return 0;
+  
+}
